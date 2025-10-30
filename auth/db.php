21018e8e2469fae53ca96214f82488960c1d33d5
Santiago Molina <nsmolinal@academia.usbbog.edu.co>
2025-10-29 20:14:48 -0500

    arreglo de bases datos 1

--- auth/db.php
+++ auth/db.php
@@ -1,9 +1,8 @@
 <?php
 $host = "localhost"; // En hosting compartido suele ser localhost
-$db_name = "artesanias"; // Probablemente: usuario_nombrebd
-$username = "root"; // Tu usuario de BD (puede ser diferente al del sitio)
-$password = "0525"; // Tu contraseña de BD (puede ser diferente)
-
+$db_name = "gurubackend_artesanias"; // Probablemente: usuario_nombrebd
+$username = "gurubackend"; // Tu usuario de BD (puede ser diferente al del sitio)
+$password = "KxXOSG38nSA5nvAUlD8M"; // Tu contraseña de BD (puede ser diferente)
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
